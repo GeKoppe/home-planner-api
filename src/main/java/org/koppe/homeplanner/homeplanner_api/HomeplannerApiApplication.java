54ea276a8ac6ba3e35243e7b43ee8b3c82dcc392
GeKoppe <gkoppe@Ilostthegame.localdomain>
2025-12-19 18:34:14 +0100

    Initial commit

---
+++ src/main/java/org/koppe/homeplanner/homeplanner_api/HomeplannerApiApplication.java
@@ -0,0 +1,13 @@
+package org.koppe.homeplanner.homeplanner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeplannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeplannerApiApplication.class, args);
+	}
+
+}
